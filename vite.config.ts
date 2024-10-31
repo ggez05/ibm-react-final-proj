ff2ddfbc923ec1cd3752572aefb466278ca505a2
Garvit <garvit510bhatia@gmail.com>
2024-10-31 15:24:10 +0530

    vite fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   server: {
     watch: { usePolling: true },
   },
-  base: "/ibm-react-final-proj",
+  base: "/ibm-react-final-proj/",
   plugins: [
     react({
       include: "**/*.tsx",
